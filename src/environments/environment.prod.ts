272e6f67de646f8d0547656a82ea46831ce33ade
Gabriel Diaz <gabidiaz4231@gmail.com>
2026-07-20 21:56:26 -0300

    Me mudo a Supabase y Render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tienda-backend-production-043b.up.railway.app',
+  apiUrl: 'https://tienda-backend-18ev.onrender.com',
   whatsappNumber: '5493534014165',
 };
